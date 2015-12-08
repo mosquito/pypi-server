da785aab6114a7fcb425d4a7693dbd9fb95aa1ad
Dmitry Orlov <me@mosquito.su>
2015-12-09 01:47:36 +0300

    [fix] add classifiers

--- pypi_server/__init__.py
+++ pypi_server/__init__.py
@@ -14,7 +14,7 @@ author_info = [
     ("Dmitry Orlov", "me@mosquito.su")
 ]
 
-version_info = (0, 1, 11)
+version_info = (0, 1, 15)
 
 __version__ = ".".join(map(str, version_info))
 __author__ = ", ".join("{0} <{1}>".format(*author) for author in author_info)
