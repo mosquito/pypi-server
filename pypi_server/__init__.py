d2af1781b41f005943a34415451db0f408e89c12
Dmitry Orlov <me@mosquito.su>
2015-12-14 13:12:08 +0300

    [fix] bump version

--- pypi_server/__init__.py
+++ pypi_server/__init__.py
@@ -9,7 +9,7 @@ author_info = [
     ("Dmitry Orlov", "me@mosquito.su")
 ]
 
-version_info = (0, 1, 24)
+version_info = (0, 2, 0)
 
 __version__ = ".".join(map(str, version_info))
 __author__ = ", ".join("{0} <{1}>".format(*author) for author in author_info)
--- pypi_server/db/migrator/migrations/packagefile_basename_unique.py
+++ pypi_server/db/migrator/migrations/packagefile_basename_unique.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-from peewee import OperationalError
+from peewee import OperationalError, ProgrammingError
 from playhouse.migrate import migrate
 from pypi_server.db.migrator import migration
 
@@ -10,5 +10,5 @@ def add_uniquie_basename_index(migrator, db):
         migrate(
             migrator.add_index('packagefile', ('basename',), True)
         )
-    except OperationalError:
+    except (OperationalError, ProgrammingError):
         pass
