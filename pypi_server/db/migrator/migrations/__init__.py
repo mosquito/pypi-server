f123d4253cd8a17c24f4fb24a9df9354be5a8e7e
Dmitry Orlov <me@mosquito.su>
2015-12-30 14:29:16 +0300

    [fea] migration loading

--- pypi_server/db/migrator/migrations/__init__.py
+++ pypi_server/db/migrator/migrations/__init__.py
@@ -1,7 +1,33 @@
 # encoding: utf-8
-from pypi_server.db.migrator.migrations import create_tables
-from pypi_server.db.migrator.migrations import create_default_user
-from pypi_server.db.migrator.migrations import create_package_tables
-from pypi_server.db.migrator.migrations import packagefile_add_url_and_fetched
-from pypi_server.db.migrator.migrations import packagefile_basename_unique
-from pypi_server.db.migrator.migrations import packageversion_license_textfield
+import imp
+import os
+import re
+import sys
+import logging
+from collections import namedtuple
+
+
+log = logging.getLogger(__name__)
+MIGRATION_EXP = re.compile(r"(?P<rev>\d+)_(?P<name>.*)\.py$")
+Module = namedtuple("Module", ("name", "path", "rev"))
+
+PATH = os.path.abspath(os.path.dirname(__file__))
+
+MIGRATIONS = sorted(
+    map(
+        lambda x: Module(path=os.path.join(PATH, x.string), **x.groupdict()),
+        filter(
+            None,
+            map(
+                MIGRATION_EXP.match,
+                os.listdir(PATH)
+            )
+        )
+    ),
+    key=lambda x: x.rev
+)
+
+
+for mod in MIGRATIONS:
+    sys.modules["{}.{}".format(__name__, mod.name)] = imp.load_source(mod.name, mod.path)
+
