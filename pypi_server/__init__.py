0501bd5fa4218b6c5b9f72d9c96faf990725e040
Dmitry Orlov <me@mosquito.su>
2017-10-15 22:38:30 +0300

    [fix] bump

--- pypi_server/__init__.py
+++ pypi_server/__init__.py
@@ -9,7 +9,7 @@ author_info = [
     ("Dmitry Orlov", "me@mosquito.su")
 ]
 
-version_info = (0, 4, 1)
+version_info = (0, 4, 2)
 
 __version__ = ".".join(map(str, version_info))
 __author__ = ", ".join("{0} <{1}>".format(*author) for author in author_info)
