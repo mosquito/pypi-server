76c001fc30bea9fe3bc07d8e8863f7682e8fcdbe
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-11-14 18:46:58 +0300

    fix/migrations: postgresql
    Trying to fix the problem of applying migrations on first run (postgresql)

--- pypi_server/db/migrator/migrations/005_packagefile_basename_unique.py
+++ pypi_server/db/migrator/migrations/005_packagefile_basename_unique.py
@@ -1,11 +1,14 @@
 # encoding: utf-8
-from peewee import OperationalError, ProgrammingError
+from peewee import OperationalError, ProgrammingError, PostgresqlDatabase
 from playhouse.migrate import migrate
 from pypi_server.db.migrator import migration
 
 
 @migration(8)
 def add_uniquie_basename_index(migrator, db):
+    # Index already exists from previous migrations
+    if isinstance(db, PostgresqlDatabase):
+        pass
     try:
         migrate(
             migrator.add_index('packagefile', ('basename',), True)
